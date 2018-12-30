82ee4f6c0847ce100281010158fd8507d095288f
zhangfan <15513807952@163.com>
2018-12-30 15:14:10 +0800

    Update tianqipaqu.py
    second

--- tianqipaqu.py
+++ tianqipaqu.py
@@ -1,52 +1,37 @@
-# coding=utf-8
-import sys
+# coding = utf-8
 import requests
+import time
 import pymysql
-from bs4 import BeautifulSoup as bs
+def connect_db():
+    conn = pymysql.connect(host='localhost',
+                           port=3306,
+                           user='sql123_206_68_1',
+                           passwd='kwtJSZcWKh',
+                           db='sql123_206_68_1',
+                           charset='utf8')
+    cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
+    return cursor,conn
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
-conn = pymysql.connect(host='localhost',
-                       port=3306,
-                       user='数据库用户名',
-                       passwd='你的密码',
-                       db='数据库名',
-                       charset='utf8')
-cursor = conn.cursor(cursor=pymysql.cursors.DictCursor)
-cursor.execute('select weather_code from ins_county')
-result = cursor.fetchall()
-conn.commit()
-for i in range(len(result)):
-    citycode = result[i]['weather_code']
-    url = "http://wthrcdn.etouch.cn/WeatherApi?citykey=" + citycode
-    r = requests.get(url, verify=False)
-    soup = bs(r.text, 'html.parser')
+def get_info(citycode='101010100'):
+    url="http://t.weather.sojson.com/api/weather/city+"+citycode
+    r=requests.get(url)
+    return r.text
 
-    wendu = soup.find('wendu').text
-    fengli = soup.find('fengli').text
-    shidu = soup.find('shidu').text
-    fengxiang = soup.find('fengxiang').text
-    if soup.find('pm25'):
-        pm25 = soup.find('pm25').text
-    else:
-        pm25 = 'Null'
-    if soup.find('quality'):
-        quality = soup.find('quality').text
-    else:
-        quality = 'Null'
-    if soup.find('high_1'):
-        high = soup.find('high').text
-    else:
-        high = 'Null'
-    if soup.find('low_1'):
-        low = soup.find('low').text
-    else:
-        low = 'Null'
-    weather_content = '温度:' + wendu + '℃' + '\n风力:' + fengli + '\n适度:' + shidu + '\n风向:' + fengxiang + '\nPM2.5:' + pm25 + '\n空气质量:' + quality + '\n温度范围:' + low + '-' + high + '℃'
-    sql_exe = 'UPDATE ins_county SET weather_info = \''\
-              + weather_content + '\' WHERE weather_code = ' + citycode +' '
-    print(sql_exe)
-    cursor.execute(sql_exe)
+def update_info():
+    cursor,conn=connect_db()
+    cursor.execute('select city_code from ins_county')
+    listcodes=cursor.fetchall()
     conn.commit()
-cursor.close()
-conn.close()
+    for citycode in listcodes:
+        time.sleep(0.3)
+        info=get_info(citycode['county_name'])
+        sql="UPDATE ins_county SET weather_info='%s' WHERE county_name=%s"%(info,citycode['county_name'])
+        try:
+            cursor.execute(sql)
+            conn.commit()
+            print("{}写入成功".format(citycode['county_name']))
+        except:
+            conn.rollback()
+
+if __name__ == "__main__":
+    update_info()
